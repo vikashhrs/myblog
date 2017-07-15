dc9c77e301a36e553dc66b0d4fd5929442c852b9
Vikash Kumar Sharma <vikashhrs@gmail.com>
2017-07-15 13:27:30 +0530

    fixes

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get("/dashboard",function(req,res){
     res.status(200).sendFile(__dirname + '/public/dashboard.html');
 });
 
+app.get("/designtest",function(req,res){
+    console.log(__dirname);
+    res.status(200).sendFile(__dirname + '/public/designtest.html');
+});
+
 app.listen(PORT,function(){
 	console.log("Server running on port 3000");
 })
